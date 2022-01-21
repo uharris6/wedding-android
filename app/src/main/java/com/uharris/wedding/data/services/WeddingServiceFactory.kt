5c3cbb5852686a46b1704ad3849d59d121a4a35e
Joaquin Bordes Monroig <bordesmonroigj@gmail.com>
2022-01-21 12:26:57 -0500

    Trust All Certificates

--- app/src/main/java/com/uharris/wedding/data/services/WeddingServiceFactory.kt
+++ app/src/main/java/com/uharris/wedding/data/services/WeddingServiceFactory.kt
@@ -1,5 +1,6 @@
 package com.uharris.wedding.data.services
 
+import android.annotation.SuppressLint
 import com.google.gson.Gson
 import com.uharris.wedding.BuildConfig
 import okhttp3.OkHttpClient
@@ -7,7 +8,13 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 internal object WeddingServiceFactory {
 
@@ -15,27 +22,42 @@ internal object WeddingServiceFactory {
     private const val READ_TIMEOUT = 120L
 
     inline fun <reified T> makeService(isDebug: Boolean): T {
-        val okHttpClient = makeOkHttpClient(
-                makeLoggingInterceptor((isDebug)))
+        val okHttpClient = makeTrustAllOkHttpClient(makeLoggingInterceptor((isDebug)))
         return makeService(okHttpClient, Gson())
     }
 
     inline fun <reified T> makeService(okHttpClient: OkHttpClient, gson: Gson): T {
         val retrofit = Retrofit.Builder()
-                .baseUrl(BuildConfig.BASE_URL)
-                .client(okHttpClient)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
         return retrofit.create(T::class.java)
     }
 
-    fun makeOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    private fun makeBaseOkHttpClientBuilder(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient.Builder {
         return OkHttpClient.Builder()
-                .addInterceptor(httpLoggingInterceptor)
-                .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
-                .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
-                .build()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+    }
+
+    /**
+     * This is insecure because certificate verification is not being performed.
+     * Please update https://qa-test-backend.reign.dev certificates and use makeOkHttpClient instead
+     */
+    fun makeTrustAllOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val trustAllCerts = makeTrustAllCerts()
+        val sslContext = makeTrustAllSSLContext(trustAllCerts)
+        return makeBaseOkHttpClientBuilder(httpLoggingInterceptor)
+            .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+    }
+
+    fun makeOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return makeBaseOkHttpClientBuilder(httpLoggingInterceptor).build()
     }
 
     fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
@@ -47,4 +69,24 @@ internal object WeddingServiceFactory {
         }
         return logging
     }
+
+    private fun makeTrustAllSSLContext(trustAllCerts: Array<TrustManager>): SSLContext =
+        SSLContext.getInstance("SSL").apply { init(null, trustAllCerts, SecureRandom()) }
+
+    private fun makeTrustAllCerts() = arrayOf<TrustManager>(
+        @SuppressLint("CustomX509TrustManager")
+        object : X509TrustManager {
+            @SuppressLint("TrustAllX509TrustManager")
+            @Throws(CertificateException::class)
+            override fun checkClientTrusted(chain: Array<X509Certificate?>?, authType: String?) {
+            }
+
+            @SuppressLint("TrustAllX509TrustManager")
+            @Throws(CertificateException::class)
+            override fun checkServerTrusted(chain: Array<X509Certificate?>?, authType: String?) {
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate?> = arrayOf()
+        }
+    )
 }
